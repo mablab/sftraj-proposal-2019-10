530169507fa75c4a371d7d3e21e8384737d21634
Mathieu Basille <basille@ufl.edu>
2019-10-02 20:09:21 -0400

    Team, problem, proposal, timeline.

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,8 +1,9 @@
-proposal<-"Boilerplate ISC Proposal"
+proposal<-"sftraj: A central class for tracking and movement data"
+sponsor <- "R Consortium's ISC"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+author<-"Mathieu Basille"
 
-rmarkdown::render(proposal.file, output_format="html_document",
-                  output_dir="out", quiet=TRUE)
 rmarkdown::render(proposal.file, output_format="pdf_document",
+                  output_dir="out", quiet=TRUE)
+rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
